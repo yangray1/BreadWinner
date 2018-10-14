7dee79dbaf61f87a9c972a14677e4eefa5a53d0c
crystalyip <crystal.yip@mail.utoronto.ca>
2018-10-13 22:46:39 -0400

    Added a draft of the front-end template

---
+++ Front-end-template/app/src/main/java/com/example/crystalyip/csc301/SpaceGalleryActivity.java
@@ -0,0 +1,157 @@
+package com.example.crystalyip.csc301;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by Chike on 2/12/2017.
+ */
+
+public class SpaceGalleryActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_gallery);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout); // drawer layout contains the menu items
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // this will call a problem with rotating
+        // when you rotate, destroy activity and create a new one
+        // need this if statement
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_popular_menus);
+        }
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_images);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        SpaceGalleryActivity.ImageGalleryAdapter adapter = new SpaceGalleryActivity.ImageGalleryAdapter(this, SpacePhoto.getSpacePhotos());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.nav_search:
+                Intent galleryIntent = new Intent(SpaceGalleryActivity.this, MainActivity.class);
+                startActivity(galleryIntent);
+                break;
+            case R.id.nav_active:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FoodNearMeFragment()).commit();
+                break;
+
+        }
+
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>  {
+
+        @Override
+        public ImageGalleryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            // Inflate the layout
+            View photoView = inflater.inflate(R.layout.item_photo, parent, false);
+
+            ImageGalleryAdapter.MyViewHolder viewHolder = new ImageGalleryAdapter.MyViewHolder(photoView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ImageGalleryAdapter.MyViewHolder holder, int position) {
+
+            SpacePhoto spacePhoto = mSpacePhotos[position];
+            ImageView imageView = holder.mPhotoImageView;
+
+            Glide.with(mContext)
+                    .load(spacePhoto.getUrl())
+                    .placeholder(R.drawable.ic_cloud_off_red)
+                    .into(imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return (mSpacePhotos.length);
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            public ImageView mPhotoImageView;
+
+            public MyViewHolder(View itemView) {
+
+                super(itemView);
+                mPhotoImageView = (ImageView) itemView.findViewById(R.id.iv_photo);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View view) {
+
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION) {
+                    SpacePhoto spacePhoto = mSpacePhotos[position];
+
+                    Intent intent = new Intent(mContext, SpacePhotoActivity.class);
+                    intent.putExtra(SpacePhotoActivity.EXTRA_SPACE_PHOTO, spacePhoto);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        private SpacePhoto[] mSpacePhotos;
+        private Context mContext;
+
+        public ImageGalleryAdapter(Context context, SpacePhoto[] spacePhotos) {
+            mContext = context;
+            mSpacePhotos = spacePhotos;
+        }
+
+
+    }
+}
