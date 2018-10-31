0df8a3a461ec757dc5c32ed4085628c2fb077cf9
Sean <sean.geng@mail.utoronto.ca>
2018-10-31 05:15:05 -0400

    added basic functionality of displaying listings

--- Front-end-template/app/src/main/java/com/example/crystalyip/csc301/FoodListingNearMe.java
+++ Front-end-template/app/src/main/java/com/example/crystalyip/csc301/FoodListingNearMe.java
@@ -9,29 +9,85 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FoodListingNearMe extends Fragment implements View.OnClickListener{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.menu_item, container, false);
-        ImageView directions = (ImageView) view.findViewById(R.id.food_image);
-        directions.setOnClickListener(this);
+        // reference: https://www.viralandroid.com/2016/02/android-listview-with-image-and-text.html
+        View view = inflater.inflate(R.layout.fragment_food_near_me, container, false);
+
+        String[] listviewTitle = new String[]{
+                "food",
+                "more food",
+                "even more food",
+                "even even more food"
+        };
+
+        int[] listviewImage = new int[]{
+                R.drawable.rice, R.drawable.rice, R.drawable.rice, R.drawable.rice
+        };
+
+
+
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0 ; i < 4 ; i++){
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", listviewTitle[i]);
+            hm.put("listview_image", Integer.toString(listviewImage[i]));
+            aList.add(hm);
+        }
+
+        String[] from = {"listview_title" , "listview_image"};
+        int[] to = {R.id.food_name, R.id.food_image};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),
+                aList,
+                R.layout.menu_item,
+                from, to);
+
+        ListView listingsList = view.findViewById(R.id.lstFoodList);
+        listingsList.setClickable(true);
+        listingsList.setAdapter(simpleAdapter);
+//        listingsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // id and position refer to the index of the clicked thing
+//                System.out.println("Clicked");
+//            }
+//        });
+
+
         return view;
     }
 
     @Override
     public void onClick(View v) {
+        System.out.println("iasdasnmkdasdmlasdjklasdjkl");
         switch (v.getId()) {
             case R.id.food_image:
+                System.out.println("asdasdasd");
                 // launch the map fragment
                 getFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new FragmentFoodDetail()).commit();
                 break;
         }
     }
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////
 
 //    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        // Load food listings
