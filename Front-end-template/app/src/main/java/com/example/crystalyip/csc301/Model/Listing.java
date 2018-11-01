adb11f3cf77c5c10e162851e06352ec5f3d19a67
Sean <sean.geng@mail.utoronto.ca>
2018-11-01 04:40:24 -0400

    made instance variables in Listing private, use getters

--- Front-end-template/app/src/main/java/com/example/crystalyip/csc301/Model/Listing.java
+++ Front-end-template/app/src/main/java/com/example/crystalyip/csc301/Model/Listing.java
@@ -1,15 +1,13 @@
 package com.example.crystalyip.csc301.Model;
 
-import android.widget.ImageView;
-
 public class Listing {
-    public String foodName;
-    public int listingID;
-    public String imageLink;
-    public int cookID;
-    public double price;
-    public String location;
-    public int imageID;
+    private String foodName;
+    private int listingID;
+    private String imageLink;
+    private int cookID;
+    private double price;
+    private String location;
+    private int imageID;
 
     public Listing(String foodName, int listingID, String imageLink, int cookID, double price, String location, int imageID) {
         this.foodName = foodName;
@@ -19,5 +17,33 @@ public class Listing {
         this.price = price;
         this.location = location;
         this.imageID = imageID;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public int getListingID() {
+        return listingID;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public int getCookID() {
+        return cookID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getImageID() {
+        return imageID;
     }
 }
