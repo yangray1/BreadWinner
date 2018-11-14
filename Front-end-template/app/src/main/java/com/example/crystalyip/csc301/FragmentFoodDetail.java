b94a3e70cc3a03c7e328081fe3c2b152e40c3154
crystalyip <crystal.yip@mail.utoronto.ca>
2018-11-13 22:47:03 -0500

    Changes to food detail layout

--- Front-end-template/app/src/main/java/com/example/crystalyip/csc301/FragmentFoodDetail.java
+++ Front-end-template/app/src/main/java/com/example/crystalyip/csc301/FragmentFoodDetail.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,16 +21,16 @@ public class FragmentFoodDetail extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView =inflater.inflate(R.layout.fragment_food_detail, container, false);
         Bundle bundle = this.getArguments();
-        System.out.println("henlo0");
         if (bundle != null) {
-            System.out.println("henlo");
-            System.out.println("foodname: "+bundle.getString("imageName"));
             int imageID = bundle.getInt("imageURL");
             ImageView imageView=(ImageView) rootView.findViewById(R.id.image_food);
             imageView.setImageResource(imageID);
-            TextView tv = (TextView) rootView.findViewById(R.id.food_name);
-            System.out.println("foodname2: "+bundle.getString("imageName"));
-            tv.setText(bundle.getString("imageName"));
+            TextView tv = (TextView) rootView.findViewById(R.id.food_description);
+            String foodDescription = bundle.getString("Description");
+            SpannableString descriptions = new SpannableString(foodDescription);
+            descriptions.setSpan(new RelativeSizeSpan(1.3f), 0, foodDescription.indexOf("\n"), 0);
+            tv.setText(descriptions);
+
         }
 
         return rootView;
