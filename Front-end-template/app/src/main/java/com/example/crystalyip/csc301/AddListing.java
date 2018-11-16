ba281393ecc0d6d7fe29f5f58f7c541aa69fa5a4
Sean <sean.geng@mail.utoronto.ca>
2018-11-16 03:29:40 -0500

    added the associated java file that inflates the add_listing view

---
+++ Front-end-template/app/src/main/java/com/example/crystalyip/csc301/AddListing.java
@@ -0,0 +1,43 @@
+package com.example.crystalyip.csc301;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.crystalyip.csc301.Model.Listing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class AddListing extends Fragment implements View.OnClickListener{
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.add_listing, container, false);
+        return view;
+    }
+}
