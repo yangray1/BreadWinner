e6c5ca5ab3f30577f18593ceee3ab5bc5a5f22bc
tasbir rahman <tasbir49@gmail.com>
2018-11-18 16:46:39 -0500

    Refactored out http in Shwlistings, modified order to have new columns, add function that lets you add new order to database

--- Front-end-template/app/src/main/java/com/example/crystalyip/csc301/ShowListings.java
+++ Front-end-template/app/src/main/java/com/example/crystalyip/csc301/ShowListings.java
@@ -8,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
-import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,17 +15,13 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.example.crystalyip.csc301.HTTPInteractions.HTTPRequests;
 import com.example.crystalyip.csc301.Model.Listing;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -48,43 +43,6 @@ public class ShowListings extends Fragment implements View.OnClickListener {
     public ShowListings(String query) {
         String formattedQuery = query.replace("\\s", "+");
         searchURL = "http://18.234.123.109/api/search/" + formattedQuery;
-    }
-
-    /**
-     * Sends a GET request to the url at urlToRead, and return the string representing the response.
-     *
-     * @param urlToRead url to GET from
-     * @return String response of GET request
-     */
-    public static String getHTML(String urlToRead) throws Exception {
-        // reference: https://stackoverflow.com/questions/34691175/how-to-send-httprequest-and-get-json-response-in-android/34691486
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(urlToRead);
-
-        HttpResponse response = httpclient.execute(httpget);
-
-        if (response.getStatusLine().getStatusCode() == 200) {
-            String server_response = EntityUtils.toString(response.getEntity());
-            return server_response;
-        } else {
-            System.out.println("no response from server");
-        }
-        return "";
-    }
-
-    /**
-     * Format the string returned from a GET request to our API, ridding it of newline characters
-     * and redundant backslashes.
-     *
-     * @param apiString (JSON formatted) string to format
-     * @return correctly formatted string (that can be parsed with a JSON parser)
-     */
-    public static String formatAPIString(String apiString) {
-        String remove_new_line = apiString.replace("\\n", "\\");
-        String remove_begin_slash = remove_new_line.replace("\"{", "{");
-        String remove_end_slash = remove_begin_slash.replace("}\"", "}");
-        String remove_extra_slashes = remove_end_slash.replace("\\", "");
-        return remove_extra_slashes;
     }
 
     /**
@@ -136,12 +94,12 @@ public class ShowListings extends Fragment implements View.OnClickListener {
         String allListings = "";
 
         try {
-            allListings = getHTML(searchURL);
+            allListings = HTTPRequests.getHTTP(searchURL);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String allListingsFormatted = formatAPIString(allListings);
+        String allListingsFormatted = HTTPRequests.formatJSONStringFromResponse(allListings);
 
         final List<Listing> populatedListings = getAllListings(allListingsFormatted);
 
